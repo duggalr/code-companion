e37f631078aeffa56866e6fa93d5aabf6a613748
duggalr <duggalr42@gmail.com>
2024-10-10 13:44:16 -0400

    UI

--- api/index.py
+++ api/index.py
@@ -6,3 +6,7 @@ app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
+
+
+# TODO:
+    # add streaming functionality
--- app/components/Utils/TopNavBar.js
+++ app/components/Utils/TopNavBar.js
@@ -16,45 +16,11 @@ export default function TopNavBar () {
 
     return (
 
-        // <ul
-        // className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-700 dark:border-gray-700 dark:text-gray-400 bg-white dark:bg-neutral-900 pl-2 pr-4">
-        
-// <ul
-        //     className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b-2 border-gray-300 bg-[#F5F5F5] dark:text-gray-300 dark:border-gray-600 dark:bg-gray-800"
-        // >
-
-        // style={{ backgroundColor: "#F5F5F5" }}
-
-        // text-gray-500 
         <ul
-            // className="flex flex-wrap text-sm font-medium text-center border-b-2 border-gray-300 bg-[#F5F5F5] dark:bg-gray-900 dark:text-gray-300"
+            // className="flex flex-wrap text-sm font-medium text-center border-b-2 border-gray-300 bg-[#F3F4F6] dark:bg-gray-900 dark:text-gray-300"
             className="flex flex-wrap text-sm font-medium text-center border-b-2 border-gray-300 bg-[#F3F4F6] dark:bg-gray-900 dark:text-gray-300"
-            // className="flex flex-wrap text-sm font-medium text-center border-b-2 border-gray-300 bg-[#eff0f3] dark:bg-gray-900 dark:text-gray-300"
         >
-                
             <li>
-                {/* className="inline-block pt-2.5 pl-2 rounded-t-lg text-[16px] text-black dark:text-gray-100 cursor-pointer tracking-normal" */}
-                {/* <a
-                href="http://localhost:3000/landing"
-                // className="inline-block pt-2.5 pl-2 pb-2 rounded-t-lg text-[16px] text-black cursor-pointer tracking-tight"
-                
-                // className="inline-block pt-2.5 pl-2 pb-2 rounded-t-lg text-[16px] cursor-pointer tracking-tight "
-                className={`${pacifico_font.className} inline-block pt-2.5 pl-2 pb-2 rounded-t-lg text-[17px] cursor-pointer tracking-widest`}
-                // style={{ color: "#2E2E2E" }}
-                // style={{ color: "black" }}
-                > */}
-                    {/* <FontAwesomeIcon
-                        icon={faLaptopCode}
-                        size="sm"
-                        className="pr-2 pl-2"
-                    /> */}
-
-                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" className="pr-2 pl-2 fill-zinc-900 dark:fill-white">
-                        <path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/>
-                    </svg>
-                    Code Companion
-                </a> */}
-
                 <a
                 href="http://localhost:3000/landing"
                 className={`${pacifico_font.className} scroll-m-20 text-[18px] mt-1 ml-3 tracking-widest flex items-center text-zinc-900 dark:text-gray-300`}>
@@ -63,35 +29,20 @@ export default function TopNavBar () {
                     </svg>
                     Code Companion
                 </a>
-
             </li>
             <div className="ml-auto flex items-center space-x-6">
-                {/* <a href="#" className="p-2.5 text-black font-normal dark:text-gray-300 text-[14px]"> */}
-                    {/* <FontAwesomeIcon
-                        icon={faCircleInfo}
-                        size="sm"
-                        className="pr-1 pl-2"
-                    /> */}
-                    {/* <FontAwesomeIcon 
-                        icon={faInfo}
-                        size="sm"
-                        className="pr-2 pl-0"
-                    /> */}
-                    {/* about
-                </a> */}
-
-                <a href="https://github.com/duggalr/code-companion" className="p-2.5 pr-4 text-black font-normal dark:text-gray-300 text-[14px]">
-                    {/* <FontAwesomeIcon 
-                        icon={faGithub}
-                        size="sm"
-                        className="pr-2 pl-0"
-                    /> */}
+                <a
+                    href="https://github.com/duggalr/code-companion"
+                    // className="p-2.5 pr-4 text-black font-normal dark:text-gray-300 text-[14px]"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center text-gray-600 hover:text-gray-800 dark:text-gray-300 text-[13px] space-x-0"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 30 30" className="mr-1 fill-zinc-900 dark:fill-white">
+                        <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+                    </svg>
                     github
                 </a>
-
-                {/* <a href="#" className="p-2.5 text-black dark:text-gray-400">
-                    Link 2
-                </a> */}
                 <ThemeToggle />
             </div>
         </ul>
