2ac576162d0261797e38ba2b613b94ea224bb227
duggalr <duggalr42@gmail.com>
2024-10-09 22:45:20 -0400

    UI

--- app/components/MainRightSide/ConsoleChatTabs.js
+++ app/components/MainRightSide/ConsoleChatTabs.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import ConsoleOutput from "./ConsoleOutput";
 import ChatInterface from "./ChatInterface";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTerminal, faComments } from '@fortawesome/free-solid-svg-icons';
 
 const ConsoleChatTabs = ({ consoleOutput }) => {
   const [activeTab, setActiveTab] = useState("console");
@@ -20,7 +22,12 @@ const ConsoleChatTabs = ({ consoleOutput }) => {
                   className={`inline-block p-0 px-6 pb-2 pt-3 text-gray-600 dark:text-gray-500 dark:hover:text-gray-300 hover:text-gray-700 rounded-t-lg active dark:border-blue-500 cursor-pointer ${activeTab === "console" ? "border-b-2 border-gray-300 text-gray-800 dark:text-blue-500 dark:border-blue-500" : ""}`}
                   aria-current="page"
                   onClick={() => setActiveTab("console")}
-                  >
+                  >   
+                    <FontAwesomeIcon 
+                      icon={faTerminal}
+                      size="sm"
+                      className="pr-2 pl-0 text-gray-800 dark:text-gray-400"
+                    />
                     Console
                   </a>
               </li>
@@ -29,6 +36,11 @@ const ConsoleChatTabs = ({ consoleOutput }) => {
                   class={`inline-block p-0 px-6 pb-2 pt-3 rounded-t-lg text-gray-600 dark:text-gray-500 hover:text-gray-700 hover:border-gray-300 dark:hover:text-gray-300 cursor-pointer ${activeTab === "chat" ? "border-b-2 border-gray-300 text-gray-800 dark:text-blue-500 dark:border-blue-500 " : ""}`}
                   onClick={() => setActiveTab("chat")}
                   >
+                    <FontAwesomeIcon 
+                      icon={faComments}
+                      size="sm"
+                      className="pr-2 pl-0 text-gray-800 dark:text-gray-400"
+                    />
                     Chat
                   </a>
               </li>
--- app/components/Utils/TopNavBar.js
+++ app/components/Utils/TopNavBar.js
@@ -34,7 +34,7 @@ export default function TopNavBar () {
                 
             <li>
                 {/* className="inline-block pt-2.5 pl-2 rounded-t-lg text-[16px] text-black dark:text-gray-100 cursor-pointer tracking-normal" */}
-                <a
+                {/* <a
                 href="http://localhost:3000/landing"
                 // className="inline-block pt-2.5 pl-2 pb-2 rounded-t-lg text-[16px] text-black cursor-pointer tracking-tight"
                 
@@ -42,14 +42,30 @@ export default function TopNavBar () {
                 className={`${pacifico_font.className} inline-block pt-2.5 pl-2 pb-2 rounded-t-lg text-[17px] cursor-pointer tracking-widest`}
                 // style={{ color: "#2E2E2E" }}
                 // style={{ color: "black" }}
-                >
-                    <FontAwesomeIcon
+                > */}
+                    {/* <FontAwesomeIcon
                         icon={faLaptopCode}
                         size="sm"
                         className="pr-2 pl-2"
-                    />
+                    /> */}
+
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" className="pr-2 pl-2 fill-zinc-900 dark:fill-white">
+                        <path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/>
+                    </svg>
+                    Code Companion
+                </a> */}
+
+                {/* TODO: start here by adding all core functionality and getting this complete */}
+
+                <a
+                href="http://localhost:3000/landing"
+                className={`${pacifico_font.className} scroll-m-20 text-[18px] mt-1 ml-3 tracking-widest flex items-center text-zinc-900 dark:text-gray-300`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" className="h-8 w-8 pr-2 fill-zinc-900 dark:fill-white">
+                        <path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/>
+                    </svg>
                     Code Companion
                 </a>
+
             </li>
             <div className="ml-auto flex items-center space-x-6">
                 {/* <a href="#" className="p-2.5 text-black font-normal dark:text-gray-300 text-[14px]"> */}
