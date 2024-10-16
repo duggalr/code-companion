5f3a2ffb63423f7df41347812cacb59b1c46238d
duggalr <duggalr42@gmail.com>
2024-10-15 20:27:04 -0400

    async scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
-        <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js" async></script>
         {/* <script src="https://cdn.jsdelivr.net/npm/pyodide@0.26.2/pyodide.min.js"></script> */}
       </head>
       <body className={inter.className}>{children}</body>
